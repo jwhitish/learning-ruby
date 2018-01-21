70a8b1e78ce31996f47fa760946ef4c4d1146a00
Jacob Whitish <jacobwhitish@gmail.com>
2018-01-20 20:56:44 -0800

    practice with yields in methods

---
+++ yield_practice.rb
@@ -0,0 +1,15 @@
+#practicing using yield
+
+
+def yield_name(name)
+  puts "In the method! Let's yield."
+  yield("Kim")
+  puts "In between the yields!"
+  yield(name)
+  puts "Block complete! Back in the method."
+end
+
+yield_name("Eric") { |n| puts "My name is #{n}." }
+
+# Now call the method with your name!
+yield_name("Jacob") { |n| puts "My name is #{n}"}
