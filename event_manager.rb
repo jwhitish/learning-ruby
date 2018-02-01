e9ea148201a25766141ad43ee4a9db000954ead3
Jacob Whitish <jacobwhitish@gmail.com>
2018-01-31 20:12:01 -0800

    The Odin Project event_manager project

---
+++ event_manager.rb
@@ -0,0 +1,31 @@
+#csv parser that pulls out information from a file
+
+require "csv"
+
+# def clean_zipcode(zipcode)
+#   #check for nil
+#   if zipcode.nil?
+#     zipcode = "00000"
+#   #if zip less than 5 add 0 to front until 5
+#   elsif zipcode.length < 5
+#     zipcode = zipcode.rjust 5, "0"
+#   #if zip more than 5 truncate to first 5
+#   elsif zipcode.length > 5
+#     zipcode = zipcode[0..4]
+#   #if zip is 5 digits assume ok
+#   end
+# end
+
+#refactoring the clean_zipcode mmethod
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+puts "EventManager Initialized."
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each { |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  puts "#{name} #{zipcode}"
+}
