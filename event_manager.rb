7120340ba66f831db00ed309d898aa4f1f01be4b
Jacob Whitish <jacobwhitish@gmail.com>
2018-02-01 20:28:14 -0800

    uses google api to check for rep names by zipcode

--- event_manager.rb
+++ event_manager.rb
@@ -1,6 +1,8 @@
 #csv parser that pulls out information from a file
+#env must have google-api-client installed
 
 require "csv"
+require "google/apis/civicinfo_v2"
 
 # def clean_zipcode(zipcode)
 #   #check for nil
@@ -21,11 +23,31 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+                                  address: zip,
+                                  levels: 'country',
+                                  roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislators_string = legislator_names.join(", ")
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
 puts "EventManager Initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
 contents.each { |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 }
