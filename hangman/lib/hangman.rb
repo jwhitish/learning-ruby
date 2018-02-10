db9c3dc1011185dfdacf9772575c771dd3db2ccd
jwhitish <jacobwhitish@gmail.com>
2018-02-10 09:04:24 -0800

    added hangman directory

---
+++ hangman/lib/hangman.rb
@@ -0,0 +1,6 @@
+#load the 5desk.txt dictionary
+#select a random word between 5-12 char long
+#display guess count, letters already chosen, position in word (H_ll_), and incorrect letters already chosen
+#every turn guess char, make it case insensitive, update display to reflect whether the letter was correct, if out of guesses player should lose
+#at start of turn give player option to save game or guess letter
+#when program first loads give option to open saved game
