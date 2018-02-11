c401efe7b207107b66580173b32cb622e68cd5f7
jwhitish <jacobwhitish@gmail.com>
2018-02-11 09:06:22 -0800

    pine fm exercises

---
+++ angryboss.rb
@@ -0,0 +1,4 @@
+
+puts "Whaddya want!?"
+response = gets.chomp
+puts "WHADDYA MEAN '#{response}'?!?! YOU'RE FIRED!"
---
+++ tableofcontents.rb
@@ -0,0 +1,11 @@
+lineWidth = 40
+
+puts "Table of Contents".center(lineWidth)
+puts "Chapter 1: Numbers".ljust(lineWidth/2) + "Page 1".rjust(lineWidth/2)
+
+# lineWidth = 40
+# str = '--> text <--'
+# puts str.ljust  lineWidth
+# puts str.center lineWidth
+# puts str.rjust  lineWidth
+# puts str.ljust(lineWidth/2) + str.rjust(lineWidth/2)
