dba6fcd9092376a355ce7150aa7aff013fdfcdcc
jwhitish <jacobwhitish@gmail.com>
2018-03-04 14:24:53 -0800

    playing ruby warrior for fun

---
+++ ruby_warrior.rb
@@ -0,0 +1,28 @@
+#my commands for the bloc.io ruby warrior game
+class Player
+  def play_turn(warrior)
+    @health = warrior.health
+
+    if warrior.feel.empty?
+      if @health < 20
+        if @health < @endhealth
+          if warrior.feel.empty?
+            warrior.walk!
+          else
+            warrior.attack!
+          end
+        else
+          warrior.rest!
+        end
+      else
+        warrior.walk!
+      end
+
+    else
+      warrior.attack!
+    end
+
+    @endhealth = warrior.health
+
+  end
+end
---
+++ ruby_warrior_working.rb
@@ -0,0 +1,27 @@
+class Player
+  def play_turn(warrior)
+    @health = warrior.health
+
+    if warrior.feel.empty?
+      if @health < 20
+        if @health < @endhealth
+          if warrior.feel.empty?
+            warrior.walk!
+          else
+            warrior.attack!
+          end
+        else
+          warrior.rest!
+        end
+      else
+        warrior.walk!
+      end
+
+    else
+      warrior.attack!
+    end
+
+    @endhealth = warrior.health
+
+  end
+end
