7339525f778830b6ee50c135867e02d9cebedcb1
jwhitish <jacobwhitish@gmail.com>
2018-03-16 18:00:39 -0700

    working on TOP advanced ruby building blocks project

---
+++ bubble_sort.rb
@@ -0,0 +1,25 @@
+arry = [4,3,78,2,0,2]
+
+def bubble_sort(array)
+  #compare two elements
+  #if left is bigger than right, swap places
+  #do this until no swaps needed
+  sorted = false
+  while sorted == false
+    array.each_index do |index|
+      if index < (array.length - 1)
+        if array[index] > array[index + 1]
+          array[index], array[index + 1] = array[index + 1], array[index]
+        end
+      else
+        #fix this shit
+        if array == array.sort
+          sorted = true
+        end
+      end
+    end
+  end
+  puts array.join(",")
+end
+
+bubble_sort(arry)
