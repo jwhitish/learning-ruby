a62e33ee716edce8da2a08feb945456368204846
jwhitish <jacobwhitish@gmail.com>
2018-03-20 20:56:00 -0700

    notes

--- notes.rb
+++ notes.rb
@@ -19,3 +19,15 @@ class Square #new class
 end
 
 -Reflection is the process by which a program can inspect, analyze, and modify itself while its running.
+-Protected methods make a method private, but within the scope of the class rather than locally to the object
+
+-Polymorphism is the concept of writing code that can work with objects of multiple types and classes at once. ie, the + method works to add numbers, join strings, and add arrays.
+
+-Nested classes, classes defined within another class. Useful for when a class depends on another class that isnt necessarily useful anywhere else. To access nested class from outside the parent class you use ::
+ie
+class Drawing
+  class line
+  end
+end
+
+a = Drawing::line.new
