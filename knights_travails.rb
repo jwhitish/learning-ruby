b804d9cc5544ea1b7d08316951d09de4fe54ccaf
jwhitish <jacobwhitish@gmail.com>
2018-04-20 07:05:24 -0700

    setting up file for knights travails as TOP cs project

---
+++ knights_travails.rb
@@ -0,0 +1,21 @@
+
+def create_board
+  #create the game board (8x8)
+  #place the knight on the board
+end
+
+def print_board
+  #prints out the board
+end
+
+def build_move_tree
+  #builds the binary tree of possible moves
+end
+
+def knight_moves(orig, dest)
+  #Treat all possible moves the knight could make as children in a tree (there are a max of 8 from any given square)
+  #Don’t allow any moves to go off the board.
+  #Use BFS
+  #Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square.
+  #Output what that full path looks like
+end
