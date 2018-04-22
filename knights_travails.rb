a3b6f33e5a6d58f2653f82fc3eb30e7db934103f
jwhitish <jacobwhitish@gmail.com>
2018-04-22 11:02:08 -0700

    Still working on knights travails

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,37 +1,18 @@
-class Node
-  #occupied or unoccupied?
-  #should have top, bottom, l, r connections
-  #should have coordinate value
-end
+class Square
+  attr_reader :x, :y, :parent_obj, :children
 
-class Game
-  attr_accessor #vars here
-
-  def initialize
+  def initialize(x, y, parent_obj = nil)
+    @x = x
+    @y = y
+    @parent_obj = parent_obj
+    @children = []
   end
 
-  def create_board
-    #create the game board (8x8)
-    #squares are nodes
-    #place the knight on the board
-  end
+  def make_children
 
-  def print_board
-    #prints out the board
-  end
-
-  def build_move_tree
-    #builds the binary tree of all possible moves for the knight
-  end
-
-  def knight_moves(orig, dest)
-    #create the knight object
-    #Treat all possible moves the knight could make as children in a tree (there are a max of 8 from any given square)
-    #Don’t allow any moves to go off the board.
-    #Use BFS
-    #Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square.
-    #Output what that full path looks like
   end
 end
+
+
 
 my_game = Game.new
