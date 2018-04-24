47c10706145d617a0038697c9663c4df76e1509e
jwhitish <jacobwhitish@gmail.com>
2018-04-24 06:18:38 -0700

    Learning to test with rspec

---
+++ testing_lesson/spec/run_logger_spec.rb
@@ -0,0 +1,18 @@
+#for example only
+require "run_logger" #file doesn't exist
+
+describe Run do
+
+  describe "attributes" do
+    subject do    #declaring subject as instance of class Run
+    end
+      Run.new(:duration => 32,
+              :distance => 5.2
+              :timestamp => "2014-12-22 20:30")
+    end
+
+    it { is_expected.to respond_to(:duration) }
+    it { is_expected.to respond_to(:distance) }
+    it { is_expected.to respond_to(:timestamp) }
+  end
+end
